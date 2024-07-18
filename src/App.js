33069a8bd1b12ebb02d59af2a63797cb2d7aa283
Artem <artem@MacBook-Air-Artem.local>
2024-07-18 15:33:44 +0300

    test

--- src/App.js
+++ src/App.js
@@ -14,24 +14,31 @@ function App() {
   };
 
   const user = tg.initDataUnsafe?.user;
-  const profilePhotoUrl = user?.photo_url;
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {profilePhotoUrl && (
+        {user?.photo_url && (
           <div className="profile-photo-container">
             <img
-              src={profilePhotoUrl}
+              src={user.photo_url}
               className="profile-photo"
               alt="User Profile"
             />
           </div>
         )}
-        <p>Привет - {user?.username}</p>
-        <p>Твой АЙДИ - {user?.id}</p>
-
+        <div className="user-info">
+          <p>ID: {user?.id}</p>
+          <p>Логин: {user?.username}</p>
+          <p>Имя: {user?.first_name}</p>
+          <p>Фамилия: {user?.last_name}</p>
+          <p>Язык: {user?.language_code}</p>
+          {user?.is_premium && <p>Премиум пользователь</p>}
+          {user?.is_bot && <p>Это бот</p>}
+          {user?.added_to_attachment_menu && <p>Добавлен в меню вложений</p>}
+          {user?.allows_write_to_pm && <p>Разрешены сообщения в личку</p>}
+        </div>
         <button onClick={onClose}>закрыть</button>
       </header>
     </div>
