0440f237d56c6625e5d85d9884ecb566f0e352f0
Artem <artem@MacBook-Air-Artem.local>
2024-07-18 15:09:15 +0300

    test

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+const tg = window.Telegram.WebApp;
+function App() {
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
+  const onClose = () => {
+    tg.close();
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>ДИКИИИП ПРИВЕТ!!!!!</p>
+        <button onClick={onClose}>закрыть</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
